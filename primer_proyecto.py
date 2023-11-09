373d2485f755ba3136934d552f2ea1e11660bf99
stefanvlad27 <stefan.infor11@gmail.com>
2023-11-09 14:53:53 +0100

    tema 1

---
+++ primer_proyecto.py
@@ -0,0 +1,8 @@
+# Generador de nombres de cerveza
+pregunta1 = input('pregunta1:')
+pregunta2 = input('pregunta2:')
+
+print(f"La cerveza se llama \n \"pregunta1 , pregunta2\"")
+      + pregunta1 + " " + pregunta2 + "\"")
+
+
