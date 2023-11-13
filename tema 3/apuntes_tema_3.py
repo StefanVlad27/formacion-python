9e9d896fca791f7ca8d34469cce655334225117a
stefanvlad27 <stefan.infor11@gmail.com>
2023-11-13 17:03:57 +0100

    dia 13/11

--- tema 3/apuntes_tema_3.py
+++ tema 3/apuntes_tema_3.py
@@ -106,12 +106,14 @@ print(resultado)
 frase = "Especialmente en las comunicaciones electrónicas, la escritura enteramente en mayúsculas equivale a gritar."
 print(frase.upper())
 
-# EJERCICIO 2:Une la siguiente lista en un string, separando cada elemento con un espacio. Utiliza el método apropiado de listas/strings, y muestra en pantalla el resultado.
+# EJERCICIO 2:Une la siguiente lista en un string, separando cada elemento con un espacio.
+# Utiliza el método apropiado de listas/strings, y muestra en pantalla el resultado.
 lista_palabras = ["La","legibilidad","cuenta."]
 unir=" ".join(["La","legibilidad","cuenta."])
 print(unir)
 
-# EJERCICIO 3: Reemplaza en la siguiente frase:  "Si la implementación es difícil de explicar, puede que sea una mala idea." los siguientes pares de palabras: "difícil" --> "fácil" "mala" --> "buena" y muestra en pantalla la frase con ambas palabras modificadas.
+# EJERCICIO 3: Reemplaza en la siguiente frase:  "Si la implementación es difícil de explicar, puede que sea una mala idea."
+#los siguientes pares de palabras: "difícil" --> "fácil" "mala" --> "buena" y muestra en pantalla la frase con ambas palabras modificadas.
 
 texto = "Si la implementación es difícil de explicar, puede que sea una mala idea."
 resultado = texto.replace("difícil", "fácil").replace("mala", "buena")
@@ -145,7 +147,8 @@ n1 = "SteLSFKJÑLSJCÑAL"
 print(len(n1))
 
 
-# EJERCICIO 1: Concatena 15 veces el texto "Repetición" y muestra el resultado en pantalla. Por suerte, conoces que los strings son multiplicables y puedes realizar esta actividad de forma simple y elegante.
+# EJERCICIO 1: Concatena 15 veces el texto "Repetición" y muestra el resultado en pantalla. Por suerte, conoces
+# que los strings son multiplicables y puedes realizar esta actividad de forma simple y elegante.
 a = "Repetición"
 print(a*15)
 
@@ -159,4 +162,102 @@ print("agua" not in a)
 a = "electroencefalografista"
 print(len(a))
 
-# LISTAS:
+# LISTAS: secuencia ordenada de objetos., da igual el tipo. va entre corchetes [] y separados por coma , . El orden de los elementos puede variar.
+mi_lista = ['a', 'b', 'c']
+print(type(mi_lista))
+
+mi_lista2 = [142, 'hola', 2342.4565] # pueden almacenar objetos de diferentes tipos
+mi_lista3 = mi_lista + mi_lista2 # las listas se pueden concatenar
+mi_lista3[0] = 'stefan' # en la posicion 0 del indice , modificame el campo que haya por el texto
+print(mi_lista3)
+
+print(len(mi_lista3)) # tamaño de la lista
+
+# MÉTODOS LISTAS
+
+mi_lista3.append('g') # .append añade valores a la lista
+print(mi_lista3)
+
+mi_lista3.pop() # .pop elimina elementos de la lista. Sin parámetros, elimina el último registro.
+print(mi_lista3)
+
+mi_lista3.pop(3) # .pop elimina elementos de la lista. Debemos indicar el índice de la lista del elemento a eliminar.
+print(mi_lista3)
+
+eliminado = mi_lista3.pop(2) # puedes guardar el elemtento eliminado
+print(eliminado)
+
+desordenado = ['s', 'g', 'o', 'a', 'z']
+desordenado.sort() # .sort ordena alfabeticamente o numericamente los valores
+print(desordenado)
+# MUY IMPORTANTE: .sort reordena la lista que existe, NO crea algo a partir de lo que ya hay.
+# Si metemos el resultado en una variable, nos dará error. Para ver el resultado, volver a llamar
+# a la variable ordenada.
+
+desordenado.reverse() # ordena pero al revés, de la Z -> A y 9 -> 0.
+print(desordenado)
+
+
+# EJERCICIO 1: Crea una lista con 5 elementos, dentro de la variable mi_lista.
+# Puedes incluir strings, booleanos, números, etc.
+mi_lista = ['a', 'b', 'c', 12345, 392.42]
+
+# EJERCICIO 2: Agrega el elemento "motocicleta" a la siguiente lista de medios de transporte:
+medios_transporte = ["avión", "auto", "barco", "bicicleta"]
+medios_transporte.append('motocicleta')
+
+# EJERCICIO 3: Utiliza el método pop() para quitar el tercer elemento de la siguiente lista llamada frutas, y almacénalo en una variable llamada eliminado.
+# Utiliza métodos de listas sin alterar la línea de código ya suministrada.
+frutas = ["manzana", "banana", "mango", "cereza", "sandía"]
+eliminado = frutas.pop(2)
+
+
+
+# DICCIONARIOS: {'clave':'valor','arte':'cine'} -> pares de palabras agrupados, la primera se denomina clave y la segunda valor. Van entre {}
+diccionario = {'c1':'valor1', 'c2':'valor2'}
+print(diccionario)
+
+resultado = diccionario['c1'] # esta es la forma de realizar búsquedas de claves en un diccionario
+print(resultado)
+
+cliente = {'nombre':'stefan', 'apellido':'vlad', 'edad':22, 'peso':85.5}
+consulta = cliente['apellido']
+print(consulta)
+
+dic = {'c1':55, 'c2':[10, 20, 30],'c3':{'s1':100, 's2':200}} # diccionario creado pero con INT, lista y otro diccionario dentro
+print(dic['c2'][2]) # busca dentro de la clave 'c2' del diccionario, el segundo parámetro
+print(dic['c3']['s2']) # busca dentro de la clave 'c3' del diccionario la clave 's2' de ese diccionario
+
+dic = {'c1':['a','b','c'], 'c2':['d','e','f']}
+print(dic['c2'][1].upper()) # Muestra resultado en mayuscula
+
+cliente = {'nombre':'stefan', 'apellido':'vlad', 'edad':22, 'peso':85.5}
+cliente['nacionalidad'] = 'rumania' # para añadir valores nuevos al diccionario
+print(cliente)
+
+cliente = {'nombre':'stefan', 'apellido':'vlad', 'edad':22, 'peso':85.5}
+cliente['nombre'] = 'klk' # para modificar valores existentes del diccionario
+print(cliente)
+
+# MÉTODOS
+print(cliente.keys()) # imprime solamente las claves del diccionario
+print(cliente.values()) # imprime solamente los valores del diccionario
+print(cliente.items()) # muestra los tupples
+
+
+# Crea un diccionario llamado mi_dic que almacene la siguiente información de una persona:
+#     nombre: Karen
+#     apellido: Jurgens
+#     edad: 35
+#     ocupacion: Periodista
+# Los nombres de las claves y valores deben ser iguales a la consigna.
+
+#nombre: Karen
+#apellido: Jurgens
+#edad: 35
+#ocupacion: Periodista
+mi_dic = {'nombre':'Karen','apellido':'Jurgens','edad':35,'ocupacion':'Periodista'}
+
+# Crea una función print que devuelva del segundo item de la lista llamada points2, dentro del siguiente diccionario.
+mi_dict = {"valores_1":{"v1":3,"v2":6},"puntos":{"points1":9,"points2":[10,300,15]}}
+print(mi_dict["puntos"]["points2"][1])
