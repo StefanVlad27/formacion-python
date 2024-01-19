de0a714092f40e7a3c3532bbcb649f666401bc50
Stefan <stefanvlad@iasapiens.com>
2024-01-19 20:25:54 +0100

    dia 19/01/24

--- tema12/apuntes_tema_12_tkinter.py
+++ tema12/apuntes_tema_12_tkinter.py
@@ -1,5 +1,60 @@
 from tkinter import * # tkinter sirve para crear interfaces gráficas de usuario
 
+# funcionalidad de la calculadora
+operador = ""
+
+def click_boton(numero):
+    global operador
+    operador = operador + numero
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(END, operador)
+
+def borrar():
+    global operador
+    operador = ""
+    visor_calculadora.delete(0, END)
+
+def obtener_resultado():
+    global operador
+    resultado = str(eval(operador))
+    visor_calculadora.delete(0, END)
+    visor_calculadora.insert(0, resultado)
+    operador = ""
+
+def revisar_check():
+    x = 0
+    for c in cuadros_comida:
+        if variables_comida[x].get() == 1:
+            cuadros_comida[x].config(state=NORMAL)
+            cuadros_comida[x].delete(0, END)
+            cuadros_comida[x].focus()
+        else:
+            cuadros_comida[x].config(state=DISABLED)
+            texto_comida[x].set("0")
+        x += 1
+
+
+    for c in cuadros_bebida:
+        if variables_bebida[x].get() == 1:
+            cuadros_bebida[x].config(state=NORMAL)
+            cuadros_bebida[x].delete(0, END)
+            cuadros_bebida[x].focus()
+        else:
+            cuadros_bebida[x].config(state=DISABLED)
+            texto_comida[x].set("0")
+        x += 1
+
+
+    for c in cuadros_postres:
+        if variables_postres[x].get() == 1:
+            cuadros_postres[x].config(state=NORMAL)
+            cuadros_postres[x].delete(0, END)
+            cuadros_postres[x].focus()
+        else:
+            cuadros_postres[x].config(state=DISABLED)
+            texto_comida[x].set("0")
+        x += 1
+
 # iniciar la ventana
 aplicacion = Tk()
 
@@ -84,7 +139,8 @@ for comida in lista_comidas:
                          font=("Dosis", 12, "bold"),
                          onvalue=1,
                          offvalue=0,
-                         variable=variables_comida[contador])
+                         variable=variables_comida[contador],
+                         command=revisar_check)
     comida.grid(row=contador,
                 column=0,
                 sticky=W)
@@ -120,8 +176,8 @@ for bebida in lista_bebidas:
                          font=("Dosis", 12, "bold"),
                          onvalue=1,
                          offvalue=0,
-                         variable=variables_bebida[contador])
-
+                         variable=variables_bebida[contador],
+                         command=revisar_check)
     bebida.grid(row=contador,
                 column=0,
                 sticky=W)
@@ -157,8 +213,8 @@ for postre in lista_postres:
                          font=("Dosis", 12, "bold"),
                          onvalue=1,
                          offvalue=0,
-                         variable=variables_bebida[contador])
-
+                         variable=variables_bebida[contador],
+                         command=revisar_check)
     postre.grid(row=contador,
                 column=0,
                 sticky=W)
@@ -300,6 +356,74 @@ for boton in botones:
     boton.grid(row=0,
                column=columnas)
     columnas += 1
+
+# area de recibo
+texto_recibo = Text(panel_recibo,
+                    font=("Dosis", 12, "bold"),
+                    bd=1,
+                    width=42,
+                    height=10)
+
+texto_recibo.grid(row=0,
+                  column=0)
+
+# calculadora
+visor_calculadora = Entry(panel_calculadora,
+                          font=("Dosis", 16, "bold"),
+                          bd=1,
+                          width=32)
+
+visor_calculadora.grid(row=0,
+                          column=0,
+                          columnspan=4)
+
+# botones calculadora
+botones_calculadora = ["7", "8", "9", "+",
+                       "4", "5", "6", "-",
+                       "1", "2", "3", "x",
+                       "R", "B", "0", "/"]
+
+botones_guardados = []
+
+fila = 1
+columna = 0
+for boton in botones_calculadora:
+    boton = Button(panel_calculadora,
+                     text=boton.title(),
+                     font=("Dosis", 16, "bold"),
+                     fg="white",
+                     bg="azure4",
+                     bd=1,
+                     width=8)
+
+    botones_guardados.append(boton)
+
+    boton.grid(row=fila,
+                column=columna)
+
+    if columna == 3:
+        fila += 1
+
+    columna += 1
+    if columna == 4:
+        columna = 0
+
+botones_guardados[0].configure(command=lambda: click_boton("7"))
+botones_guardados[1].configure(command=lambda: click_boton("8"))
+botones_guardados[2].configure(command=lambda: click_boton("9"))
+botones_guardados[3].configure(command=lambda: click_boton("+"))
+botones_guardados[4].configure(command=lambda: click_boton("4"))
+botones_guardados[5].configure(command=lambda: click_boton("5"))
+botones_guardados[6].configure(command=lambda: click_boton("6"))
+botones_guardados[7].configure(command=lambda: click_boton("-"))
+botones_guardados[8].configure(command=lambda: click_boton("1"))
+botones_guardados[9].configure(command=lambda: click_boton("2"))
+botones_guardados[10].configure(command=lambda: click_boton("3"))
+botones_guardados[11].configure(command=lambda: click_boton("*"))
+botones_guardados[12].configure(command=obtener_resultado)
+botones_guardados[13].configure(command=borrar)
+botones_guardados[14].configure(command=lambda: click_boton("0"))
+botones_guardados[15].configure(command=lambda: click_boton("/"))
 
 # evitar que la talla se cierre
 aplicacion.mainloop()
